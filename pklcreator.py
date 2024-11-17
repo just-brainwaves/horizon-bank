bca8e087420262488d944f89ee7ce4d3ee55c2dc
Aditya Shrimali <dev.aditya.shrimali@gmail.com>
2024-11-17 18:03:57 +0530

    Update PKL Creator

--- pklcreator.py
+++ pklcreator.py
@@ -1,9 +1,4 @@
 import pickle
-
-# Path to the pickle file
 file_path = 'custdata.pkl'
-
-# write mode with 'wb' (write binary)
 with open(file_path, 'wb') as file:
-    # empty object ({})
     pickle.dump({}, file)
